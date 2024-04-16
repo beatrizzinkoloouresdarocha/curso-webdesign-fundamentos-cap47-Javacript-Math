5419893b1fd7ce7e08d3b40c8b1b18a8b55903a7
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-04-16 11:11:12 -0300

    acrecentei mais esta etapa coloquei pra rodar console.log(Math.floor(49.9999999))  o resultado saiu 49 concluido.

--- MATH/introducao.js
+++ MATH/introducao.js
@@ -3,4 +3,6 @@ console.log(Math.max(1,2,10,4,5))
 let arr =[1,2,10,4,5]
 console.log(Math.max(...arr))
 
-console.log(Math.round(45.49))
+console.log(Math.round(45.50000001))
+
+console.log(Math.floor(49.9999999))
